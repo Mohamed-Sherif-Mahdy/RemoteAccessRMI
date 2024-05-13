964ab57b86ab189796ca7f3e9a6bad66c4fcdf39
Mohamed Sherif <mohamed.sheriff.mahdy@gmail.com>
2024-05-13 22:56:34 +0300

    make simple GUI take a file location insted of hardcoded

--- src/Clint.java
+++ src/Clint.java
@@ -1,16 +1,40 @@
+import javax.swing.*;
 import java.rmi.Naming;
-public class Clint {  
-   private Clint() {}  
-   public static void main(String[] args) {  
-      try {  
-         IWord obj = (IWord) Naming.lookup("FindInFile"); 
-         String longestWord= obj.findLongestWord(obj.processFile("..\\file.txt"));
-         String shortestWord=obj.findShortestWord(obj.processFile("..\\file.txt"));
-         System.out.println("Longest word: " + longestWord);
-         System.out.println("Shortest word: " + shortestWord);
-      } catch (Exception e) {
-         System.out.println("Client exception: " + e.toString()); 
-         
-      } 
-   } 
+import java.io.File;
+
+public class Clint {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> createAndShowGUI());
+    }
+
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("File Selection");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton button = new JButton("Select File");
+        button.addActionListener(e -> {
+            JFileChooser fileChooser = new JFileChooser();
+            int result = fileChooser.showOpenDialog(null);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                File selectedFile = fileChooser.getSelectedFile();
+                processFile(selectedFile.getAbsolutePath());
+            }
+        });
+
+        frame.getContentPane().add(button);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private static void processFile(String filePath) {
+        try {
+            IWord obj = (IWord) Naming.lookup("FindInFile");
+            String longestWord = obj.findLongestWord(obj.processFile(filePath));
+            String shortestWord = obj.findShortestWord(obj.processFile(filePath));
+            System.out.println("Longest word: " + longestWord);
+            System.out.println("Shortest word: " + shortestWord);
+        } catch (Exception e) {
+            System.out.println("Client exception: " + e.toString());
+        }
+    }
 }
